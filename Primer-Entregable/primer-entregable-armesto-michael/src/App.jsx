ce9f281d0400266e198f5399f487751cae6e7804
Michael Armesto <armestoj.michael@gmail.com>
2023-08-16 20:18:50 -0300

    subiendo primer  entregable

--- Primer-Entregable/primer-entregable-armesto-michael/src/App.jsx
+++ Primer-Entregable/primer-entregable-armesto-michael/src/App.jsx
@@ -1,21 +1,54 @@
 import { Card } from './Card'
 import './App.css'
 import { Footer } from './Footer'
+import { useState } from 'react'
 
 
 export const App = () => {
+
+  const [nombre, setNombre] = useState('');
+  const [apellido, setApellido] = useState('');
+  const [error, setError] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleNombre = ({target}) => setNombre(target.value);
+  const handleApellido = ({target}) => setApellido(target.value);
+  
+  const handleSubmit = (e) => {
+
+    e.preventDefault();
+
+    const cleanNombre = nombre.trim().toLowerCase();
+    const cleanApellido = apellido.trim().toLowerCase();
+
+    if(cleanNombre.length <= 1 || cleanApellido.length <= 1)  {
+      setError(true);
+      setSubmitted(true);
+    } else {
+      setError(false);
+      setSubmitted(true);
+    };
+      
+  }
+
   return (
     <>
       <h1>Carga de estudiantes</h1>
-      <form className='form'>
+      <form className='form' onSubmit={handleSubmit}>
 
-        <input type="text" className='text-input' placeholder='Ingresa tu nombre'/>
-        <input type="text" className='text-input' placeholder='Ingresa tu Apellido'/>
+        <input type="text" className={`text-input ${error ? 'error' : ''}`} onChange = { handleNombre } placeholder='Ingresa tu nombre'/>
+        <input type="text" className={`text-input ${error ? 'error' : ''}`} onChange = { handleApellido } placeholder='Ingresa tu Apellido'/>
 
-        <button className='btn-submit'>Enviar</button>
+        <button type='submit' className='btn-submit'>Enviar</button>
 
       </form>
-      <Card />
+      {submitted && (
+        <Card
+          nombre={nombre}
+          apellido={apellido}
+          mensaje={error ? 'Chequea los datos ingresados' : null}
+        />
+      )}
       <Footer />
     </>
   )
--- Primer-Entregable/primer-entregable-armesto-michael/src/Card.jsx
+++ Primer-Entregable/primer-entregable-armesto-michael/src/Card.jsx
@@ -1,7 +1,22 @@
 import './card.css'
 
-export const Card = () => {
+export const Card = ({ nombre, apellido, mensaje}) => {
   return (
-    <div className="card">Soy la tarjeta</div>
+    <div className="card">
+      
+      {mensaje && (
+        <>
+          <h4>Datos NO recibidos</h4>
+          <p>{mensaje}</p>
+        </>)}
+      {!mensaje && (
+        <>
+          <h4>Datos recibidos</h4>
+          <p><strong>Nombre:</strong> {nombre}</p>
+          <p><strong>Apellido:</strong> {apellido}</p>
+          <p>Gracias por registrarte!</p>
+        </>
+      )}
+    </div>
   )
 }
